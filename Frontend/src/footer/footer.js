74347145c16835f23fc29085ce6687dbc8eb2772
Karina Joanna <kxochipam2000@alumno.ipn.mx>
2023-05-18 22:18:13 -0600

    ggg

---
+++ Frontend/src/footer/footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="container">
+                <div className="item"><img src="imgs/goblogo.png" alt="Logo gobierno" /></div>
+                <div className="item"><h3>Enlaces</h3></div>
+                <div className="item"><h3>¿Qué es gob.mx?</h3></div>
+                <div className="item"><h3>Denuncias</h3></div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ Frontend/src/secondsection/secondone.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const secondone = () => {
+    return (
+        <div class="secondnav">
+            <div class="oferta--imagen--texto">
+                <p>Instituto Politécnico Nacional</p>
+            </div>
+        </div>
+    );
+}
+
+
+export default secondone;
