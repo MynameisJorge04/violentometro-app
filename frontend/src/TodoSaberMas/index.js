3e8d41f494de2f1b24bac2d9aa7544977b1e76e7
Karina Joanna <kxochipam2000@alumno.ipn.mx>
2023-05-20 23:38:26 -0600

    assets

--- frontend/src/TodoSaberMas/index.js
+++ frontend/src/TodoSaberMas/index.js
@@ -1,16 +1,18 @@
 import React from "react";
-import './TodoSaberMas.css'
+import './TodoSaberMas.css';
+import violentometro from '../assets/violentometro.pdf';
+import violentometroBanner from '../assets/violentometro-banner.pdf';
 
-const TodoSaberMas = () =>{
-    return(
+const TodoSaberMas = () => {
+    return (
         <section className="sabermas--container">
             <div className="text--container--sa">
                 <h3>¿Te interesa saber más?</h3>
                 <p>Si te interesa conocer más acerca del violentometro a continuación puedes encontrar los links de descarga para conocer más acerca del tema.</p>
             </div>
             <div className="button-container">
-                <button onClick={() => window.open("files/violentometro.pdf", "_blank")}>Violentometro</button>
-                <button>¿Has visto el violentometro últimamente?</button>
+                <button onClick={() => window.open(violentometroBanner, "_blank")}>Violentometro</button>
+                <button onClick={() => window.open(violentometro, "_blank")}>¿Has visto el violentometro últimamente?</button>
             </div>
         </section>
     );
